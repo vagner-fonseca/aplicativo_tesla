02e13e84b1b057dd1760413ccfcf32eb3c30237b
unknown <vagner.cfonseca@gmail.com>
2021-06-03 14:42:41 -0300

    first

---
+++ component/CarList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text, FlatList, Dimensions} from 'react-native';
+import CarItem from '../CarItem'
+import styles from './styles';
+import cars from './cars';
+
+const CarList = (props) => {
+    return(
+
+        <View style={styles.container}>
+
+            <FlatList 
+            data={cars}
+            renderItem={({item}) => <CarItem car={item}/>}
+            showsHorizontalScrollIndicator={false}
+            snapToAlignment={'start'}
+            decelerationRate={'fast'}
+            snapToInterval={Dimensions.get('window').height }
+            
+            />
+
+        </View>
+    );
+};
+
+export default CarList;
+
